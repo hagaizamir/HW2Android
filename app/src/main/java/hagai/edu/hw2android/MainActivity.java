230ecb28356db222b0f714d29e2d8dd0fec9a024
hagaizamir <hagai34@gmail.com>
2017-03-23 14:21:30 +0200

    Initial commit

---
+++ app/src/main/java/hagai/edu/hw2android/MainActivity.java
@@ -0,0 +1,13 @@
+package hagai.edu.hw2android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
